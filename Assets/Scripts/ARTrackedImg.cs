9937225e722351edc902a82c74c33539833fec4d
Rafael Afonso <rafael.s.afonso@usp.br>
2021-11-01 15:41:17 -0300

    (WIP) Inclui parte do código referente à remoção dos objetos. Ainda não funcional.

--- Assets/Scripts/ARTrackedImg.cs
+++ Assets/Scripts/ARTrackedImg.cs
@@ -1,3 +1,7 @@
+// Tutoriais usados como referência: 
+// https://youtu.be/rJDGJUfxrN4
+// https://youtu.be/qIxClnigves
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +10,12 @@ using UnityEngine.XR.ARFoundation;
 
 public class ARTrackedImg : MonoBehaviour
 {
+    public float _timer;
     public ARTrackedImageManager trackedImageManager;
     public List<GameObject> _objectList = new List<GameObject>();
     private Dictionary<string,GameObject> _prefabDic = new Dictionary<string, GameObject>();
+    private List<ARTrackedImage> _trackedImg = new List<ARTrackedImage>();
+    private List<float> _trackedTimer= new List<float>();
 
     void Awake()
     {
@@ -16,6 +23,41 @@ public class ARTrackedImg : MonoBehaviour
         {
             string tName = obj.name;
             _prefabDic.Add(tName,obj);
+        }
+    }
+
+    void Update()
+    {
+        if(_trackedImg.Count > 0 )
+        {
+            List<ARTrackedImage> tNumList = new List<ARTrackedImage>();
+            for(var i = 0 ; i < _trackedImg.Count ; i++)
+            {
+                if(_trackedImg[i].trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+                {
+                    if(_trackedTimer[i] > _timer)
+                    {
+                        string name = _trackedImg[i].referenceImage.name;
+                        GameObject tObj = _prefabDic[name];
+                        tObj.SetActive(false);
+                        tNumList.Add(_trackedImg[i]);
+                    }
+                    else
+                    {
+                        _trackedTimer[i] += Time.deltaTime;
+                    }
+                }
+            }
+
+            if(tNumList.Count > 0 )
+            {
+                for ( var i = 0 ; i < tNumList.Count ; i++)
+                {
+                    int num = _trackedImg.IndexOf(tNumList[i]);
+                    _trackedImg.Remove(_trackedImg[num]);
+                    _trackedTimer.Remove(_trackedTimer[num]);
+                }
+            } 
         }
     }
 
@@ -32,11 +74,25 @@ public class ARTrackedImg : MonoBehaviour
     {
         foreach( ARTrackedImage trackedImage in eventArgs.added)
         {
-            UpdateImage(trackedImage);
+            if(!_trackedImg.Contains(trackedImage)) 
+            {
+                _trackedImg.Add(trackedImage);
+                _trackedTimer.Add(0);
+            }
         }
 
         foreach( ARTrackedImage trackedImage in eventArgs.updated)
         {
+            if(!_trackedImg.Contains(trackedImage))
+            {
+                _trackedImg.Add(trackedImage);
+                _trackedTimer.Add(0);
+            }
+            else 
+            {
+                int num = _trackedImg.IndexOf(trackedImage);
+                _trackedTimer[num] = 0;
+            }
             UpdateImage(trackedImage);
         }
     }
